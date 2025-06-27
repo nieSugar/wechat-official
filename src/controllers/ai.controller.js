7a3c0edbba5649fe7b9fa94ccfa542fcf01fa47a
NieRiFeng <17673376360@163.com>
2025-06-27 16:56:26 +0800

    使用Gemini作为ai模型

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -1,21 +1,15 @@
 import aiReplyUtil from "../utils/aiReply.util.js";
 
 export const aiReply = async (req, res) => {
-  const { content, messages } = req.body;
-  if (!content && !messages) {
+  const { content } = req.body;
+  console.log(content, "content");
+  if (!content) {
     return res.status(400).send({
       message: "请输入内容",
       code: 4000,
     });
   }
-  const message = [{ role: "user", content }];
-  let reply;
-
-  if (messages) {
-    reply = await aiReplyUtil(messages);
-  } else {
-    reply = await aiReplyUtil(message);
-  }
+  const reply = await aiReplyUtil(content);
 
   res.send({
     message: "回复成功",
